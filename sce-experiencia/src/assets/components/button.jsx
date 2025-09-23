147908200d1f7025fbe94ed1f704b4a712f58e1a
alejandrj15-dev <alejandrj15@gmail.com>
2025-09-23 17:49:19 -0500

    nuevo proyecto

--- sce-experiencia/src/assets/components/button.jsx
+++ sce-experiencia/src/assets/components/button.jsx
@@ -4,7 +4,11 @@ const ButtonOption = ({titulo, onClick}) => {
     return (
         
         <div className="btn" onClick={onClick}>
+           <div className="icono">
             <ion-icon name="home-outline"></ion-icon>
+
+           </div>
+           
             {titulo}           
             </div>
         
