4e42f0dbde02f1ee2cb7977bc8f9480234492b20
luispal7030 <I202224764@cibertec.edu.pe>
2025-09-23 17:23:17 -0500

    se crea componente para la informacion y nombre del usuario, con imagen o fondo redondo

--- sce-experiencia/src/assets/components/sidebar.jsx
+++ sce-experiencia/src/assets/components/sidebar.jsx
@@ -1,18 +1,30 @@
 import ButtonOption from "./button";
+import CardUsuario from "./utils/cardUsuario";
 import "../../css/slidebar.css";
 
 const Sidebar = () => {
-    return (
-        <div className="slidebar" >
-            <nav >pestaña lateral actualizada</nav>
-            <ButtonOption titulo={"Inicio"} onClick={()=>alert("Hola")}></ButtonOption>
-            <ButtonOption titulo={"Cotizar"}></ButtonOption>
-            <ButtonOption titulo={"Compras"}></ButtonOption>
-            <ButtonOption titulo={"Ventas"}></ButtonOption>
-            <ButtonOption titulo={"Pedidos"}></ButtonOption>
-            <ButtonOption titulo={"Gestión Cliente/Proveedor"}></ButtonOption>
-          
-        </div>
-    )
-}
+  return (
+    <div className="slidebar">
+      <nav>
+        <CardUsuario
+          urlImagen={""}
+          nombre={"JohnDoe"}
+          nombreUsuario={"johndoe"}
+        />
+      </nav>
+
+      <div>
+        <ButtonOption
+          titulo={"Inicio"}
+          onClick={() => alert("Hola")}
+        ></ButtonOption>
+        <ButtonOption titulo={"Cotizar"}></ButtonOption>
+        <ButtonOption titulo={"Compras"}></ButtonOption>
+        <ButtonOption titulo={"Ventas"}></ButtonOption>
+        <ButtonOption titulo={"Pedidos"}></ButtonOption>
+        <ButtonOption titulo={"Gestión Cliente/Proveedor"}></ButtonOption>
+      </div>
+    </div>
+  );
+};
 export default Sidebar;
