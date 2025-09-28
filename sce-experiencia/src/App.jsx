d323d7e3ad6043cc6dbb66b91dc4b7a72c6cea25
miguelhc85 <migueljosuehc123@gmail.com>
2025-09-28 11:45:58 -0500

    añadido pantalla de logeo

--- sce-experiencia/src/App.jsx
+++ sce-experiencia/src/App.jsx
@@ -1,13 +1,19 @@
 import "./css/App.css";
+import "./css/login.css";
 import Login from "./assets/components/login";
 import Inicio from "./assets/components/inicio";
 import ButtonOption from "./assets/components/button";
 import Sidebar from "./assets/components/sidebar";
+
 function App() {
   return (
     <section className="layout">
 
-      <Sidebar />
+      <Login/>
+
+       <Login nombre="Invitado" />
+
+       <Sidebar />
 
       <Inicio />
 
--- sce-experiencia/src/assets/components/login.jsx
+++ sce-experiencia/src/assets/components/login.jsx
@@ -1,8 +1,20 @@
+import React, { useState } from "react";
+import "../../css/login.css"; // ajusta la ruta según dónde tengas el archivo
 
-function Login({nombre}) {
-    return <div>Login {nombre}</div>;
-    }
+function Login() {
+  return (
+    <div className="login-page">
+      <div className="login-box">
+        <h2>Bienvenido</h2>
+        <form>
+          <input type="text" placeholder="Usuario" />
+          <input type="password" placeholder="Contraseña" />
+          <button type="submit">Iniciar sesión</button>
+        </form>
+      </div>
+    </div>
+  );
+}
 
-    
+export default Login;
 
-    export default Login;
