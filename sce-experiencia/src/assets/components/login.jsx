31404a01192daf9f945b07377498c4a42a1af219
miguelhc85 <migueljosuehc123@gmail.com>
2025-10-08 22:28:42 -0500

    Modificaciones en la pantalla de logeo con logo svg

--- sce-experiencia/src/assets/components/login.jsx
+++ sce-experiencia/src/assets/components/login.jsx
@@ -4,21 +4,53 @@ import "../../css/login.css";
 
 function Login({ validacion }) {
   const navigate = useNavigate();
+  const [empresa, setEmpresa] = useState("");
+  const [usuario, setUsuario] = useState("");
+  const [contrasena, setContrasena] = useState("");
 
-  const handleLogin = (e) => {
+  const manejarLogin = (e) => {
     e.preventDefault(); 
     validacion(true);   
     navigate("/dashboard"); 
   };
 
   return (
-    <div className="login-page">
-      <div className="login-box">
-        <h2>Bienvenido</h2>
-        <form onSubmit={handleLogin}>
-          <input type="text" placeholder="Usuario" />
-          <input type="password" placeholder="Contraseña" />
-          <button type="submit">Iniciar sesión</button>
+    <div className="pagina-login">
+      <div className="contenedor-superior-izquierdo">
+        <img src="../src/assets/img/logo.svg" alt="Logo" className="logo-empresa" />
+        
+        <form onSubmit={manejarLogin} className="formulario-login">
+
+          <div className="grupo-entrada">
+            <div className="etiqueta-entrada">R.U.C EMPRESA</div>
+            <input 
+              type="text" 
+              value={empresa}
+              onChange={(e) => setEmpresa(e.target.value)}
+            />
+          </div>
+
+          <div className="grupo-entrada">
+            <div className="etiqueta-entrada">USUARIO</div>
+            <input 
+              type="text" 
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+            />
+          </div>
+          
+          <div className="grupo-entrada">
+            <div className="etiqueta-entrada">CONTRASEÑA</div>
+            <input 
+              type="password" 
+              value={contrasena}
+              onChange={(e) => setContrasena(e.target.value)}
+            />
+          </div>
+          
+          <button type="submit" className="boton-login">
+            INICIAR SESIÓN
+          </button>
         </form>
       </div>
     </div>
