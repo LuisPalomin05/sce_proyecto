96bc1480ae443bfff9a45199b91609d35ce8f442
alejandrj15-dev <alejandrj15@gmail.com>
2025-10-15 20:40:45 -0500

    componente submenu actualizado

--- sce-experiencia/src/assets/components/Submenu.jsx
+++ sce-experiencia/src/assets/components/Submenu.jsx
@@ -1,14 +1,30 @@
- 
+import ButtonOption from "./utils/button";
+import "../../css/slidebar.css";
+import { IonIcon } from "@ionic/react";
+import { clipboardOutline, timeOutline} from "ionicons/icons";
+
 const Submenu  = () =>{
     return(
-      <div>
+    <div>
+    <div>
             
-            <h1> Este es el componente submenu</h1>
-
-
+        <h1> Este es el componente submenu</h1>
+        <div className="slidebar">
+         <div>
+        <button>
+         <ButtonOption titulo={"Lista de Espera"} icono={timeOutline} ruta={""}></ButtonOption>
+         </button>
+         <button>
+         <ButtonOption titulo={"Gestionar Pedidos"} icono={clipboardOutline} ruta={""}></ButtonOption>
+         </button>
+         </div>
+         <div>Aca ira una u otra vista</div>
+      
         </div>
-    )
 
+    </div>
+    </div>
+    )
 
 };
 export default Submenu;
--- sce-experiencia/src/assets/components/utils/button.jsx
+++ sce-experiencia/src/assets/components/utils/button.jsx
@@ -2,15 +2,26 @@ import "../../../css/buttons.css";
 import { Link } from "react-router-dom";
 import { IonIcon } from "@ionic/react";
 
-const ButtonOption = ({ titulo, icono, ruta }) => {
+const ButtonOption = ({ titulo, icono, ruta, onClickHandler }) => {
+
+  const handleClick = (e) => {
+    if (onClickHandler) {
+      e.preventDefault(); 
+      onClickHandler(); 
+    }
+  };
+
   return (
+
     <Link to={ruta} className="btnsidebar">
       <div className="icono">
+        
         <IonIcon icon={icono}></IonIcon>
       </div>
 
       {titulo}
     </Link>
+
   );
 };
 export default ButtonOption;
