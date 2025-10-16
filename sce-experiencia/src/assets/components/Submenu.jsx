966f6f2cd7c2fc1cc75d22a9ff052420a93d4131
alejandrj15-dev <alejandrj15@gmail.com>
2025-10-15 19:24:48 -0500

    componente submenu creado

---
+++ sce-experiencia/src/assets/components/Submenu.jsx
@@ -0,0 +1,16 @@
+ 
+const Submenu  = () =>{
+    return(
+      <div>
+            
+            <h1> Este es el componente submenu</h1>
+
+
+        </div>
+    )
+
+
+};
+export default Submenu;
+
+
--- sce-experiencia/src/assets/components/pages/Pedidos.jsx
+++ sce-experiencia/src/assets/components/pages/Pedidos.jsx
@@ -1,11 +1,11 @@
-
+import Submenu from "../submenu";
  
  const Pedidos = () => {
     return (     
         <div>
 
         <h1>Esta es la vista pedidos.jsx </h1>
-
+        <Submenu/> 
 
 
         </div>
