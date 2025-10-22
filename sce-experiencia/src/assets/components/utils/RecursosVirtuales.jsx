576d7737ffbfcdafdb549492987add7d5c770f6e
alejandrj15-dev <alejandrj15@gmail.com>
2025-10-22 13:07:33 -0500

    vista inicio completada

--- sce-experiencia/src/assets/components/utils/RecursosVirtuales.jsx
+++ sce-experiencia/src/assets/components/utils/RecursosVirtuales.jsx
@@ -1,33 +1,33 @@
 import { IonIcon } from "@ionic/react";
 import { searchOutline } from "ionicons/icons";
+import "../../../css/recursos.css";
+
 
 const RecursosVirtuales = () => {
   return (
     <div>
-      <table>
-     <thead>
-        <tr>
-            <th>fecha</th>
-            <th>nombre</th>
-            <th>estado</th>
-            <th>ruc</th>
-            <th>acciones</th>
-        </tr>
-       
-     </thead>
-     <tbody> 
-        <tr>
-            <td>10 de octubre</td>
-            <td>Hola</td>
-            <td>Activo</td>
-            <td>123456789</td>
-            <td>
-                <button>Editar</button>
-                <button>Eliminar</button>
-            </td>
-        </tr>
-     </tbody>
-      </table>
+      <h1 className="nr">Nuestros recursos virtuales</h1>  
+     <div className="rec">
+       <div className="rec2">
+       <ion-icon name="cloud-outline"></ion-icon>
+       <div>Backend en la nube</div>
+       </div>
+       <div className="rec2">
+       <ion-icon name="book-outline"></ion-icon>
+       <div>Libro de Reclamaciones y Formatos de Documentación</div>       
+       </div>
+       <div className="rec2">
+       <ion-icon name="logo-whatsapp"></ion-icon>
+       <div>Soporte Técnico via Mensajería</div>
+       </div>  
+       </div>
+    <div className="pie">
+    <text>Desarrollado en base a:</text><ion-icon name="logo-javascript"></ion-icon>
+    <ion-icon name="logo-react"></ion-icon>
+    </div>
+
+
+
     </div>
   );
 };
