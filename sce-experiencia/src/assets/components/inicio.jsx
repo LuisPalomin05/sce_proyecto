476545aa5e7fdb043b3f889b5efda0b58152f67e
Luis Alfredo Palomino Medina <pmpalominomedina@gmail.com>
2025-10-23 23:42:17 -0500

    estoy realizando los fixes para el codigo

--- sce-experiencia/src/assets/components/inicio.jsx
+++ sce-experiencia/src/assets/components/inicio.jsx
@@ -1,4 +1,3 @@
-import Sidebar from "./sidebar";
 import "../../css/Inicio.css";
 import { useState, useEffect } from "react";
 import { IonIcon } from "@ionic/react";
@@ -13,13 +12,8 @@ import Compras from "./pages/Compras";
 import Pedidos from "./pages/Pedidos";
 import Ventas from "./pages/Ventas";
 import PaginaInicio from "./pages/PaginaInicio";
-import logo from "../img/fondobanner.jpg"; 
-import logo2 from "../img/banner2.jpg";
-import logo3 from "../img/banner4.jpg";
-import productoA from "../img/productoA.webp";
-import productoB from "../img/productoB.webp";
-import productoC from "../img/productoC.webp";
-import productoD from "../img/productoD.webp";
+
+
 
 // para boton de whatsapp
 import { FaWhatsapp } from "react-icons/fa";
@@ -27,47 +21,15 @@ import WhatsAppButton from "../components/WhatsAppButton"; // ajusta la ruta
 
 
 
-// Datos del slider
-const slidesData = [
-  { id: 1, tipo: "inicio", imagen: logo, slogan: "" },
-  { id: 2, tipo: "producto", imagen: logo2, nombre: "", descripcion: "" },
-  { id: 3, tipo: "producto", imagen: logo3, nombre: "", descripcion: "" },
-];
-
-// Datos para el catalogo de productos
-const productosData = [
-
-  { id: 1, tipo: "producto", imagen: productoA, nombre: "TARUGO PLASTICO NARANJA 3/8",  descripcion:" Accesorios de alta calidad ideales para carpintería y construcción " },
-
-  { id: 2, tipo: "producto", imagen: productoB, nombre: "TORNILLO SPACK 3 X 16 MM (10PCS)", descripcion: " Diseñado para fijaciones seguras en maderas y otros materiales"},
-
-  { id: 3, tipo: "producto", imagen: productoC, nombre: "LLAVE DE GRIFERIA 18 EN 1 MULTIFUNCIONAL", descripcion:"Para tener una mayor eficiencia en baños y cocinas, tubo de abasto y griferia" },
-
-  { id: 4, tipo: "producto", imagen: productoD, nombre: "TUERCA MILIMETRICA M6 (10PCS)", descripcion:"Se utiliza para unir tornillos de la misma medida, es común en carpintería metálica y mobiliario" },
-  
-  // ... más productos
-];
-
-
 
 const Inicio = () => {
-  // Hooks
+
+    const [showNotificacion, setShowNotificacion] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-  const [showNotificacion, setShowNotificacion] = useState(false);
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const slide = slidesData[currentSlide];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prev) => (prev + 1) % slidesData.length);
-    }, 6000);
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <div className="inicio-container">
-      <Sidebar />
+
 
       <div className="inicio-content">
         <nav className="navbar">
@@ -94,44 +56,6 @@ const Inicio = () => {
           </div>
         </nav>
 
-        {/* Slider con fondo */}
-        <div className="slider-container">
-          <div
-            className="slider-background"
-            style={{ backgroundImage: `url(${slide.imagen})` }}
-          >
-            <div className="slider-text">
-              {slide.tipo === "inicio" && <h2>{slide.slogan}</h2>}
-              {slide.tipo === "producto" && (
-                <>
-                  <h3>{slide.nombre}</h3>
-                  <p>{slide.descripcion}</p>
-                </>
-              )}
-            </div>
-          </div>
-
-          {/* Puntos de navegación */}
-          <div className="slider-dots">
-            {slidesData.map((_, idx) => (
-              <span
-                key={idx}
-                className={idx === currentSlide ? "active" : ""}
-                onClick={() => setCurrentSlide(idx)}
-              ></span>
-            ))}
-          </div>
-        </div>
-            {/* Catálogo de productos */}
-      <section className="catalogo-productos">
-      {productosData.map((prod) => (
-        <div className="producto-card" key={prod.id}>
-          <img src={prod.imagen} alt={prod.nombre} />
-          <h4>{prod.nombre}</h4>
-          <p>{prod.descripcion}</p>
-        </div>
-      ))}
-      </section>
 
         <section className="main-content">
           <Routes>
--- sce-experiencia/src/assets/components/pages/PaginaInicio.jsx
+++ sce-experiencia/src/assets/components/pages/PaginaInicio.jsx
@@ -1,27 +1,98 @@
 import "../../../css/paginainicio.css";
-import logo from "../../img/torquelogo.jpg";
-import Ranking from "../utils/Ranking.jsx";
+import { useState, useEffect } from "react";
+import { IonIcon } from "@ionic/react";
+import { notifications, notificationsOutline } from "ionicons/icons";
 
+
+
+
+
+// importaciones de assets imagenes
+import logo from '../../img/fondobanner.jpg'; 
+import logo2 from "../../img/banner2.jpg";
+import logo3 from "../../img/banner4.jpg";
+import productoA from "../../img/productoA.webp";
+import productoB from "../../img/productoB.webp";
+import productoC from "../../img/productoC.webp";
+import productoD from "../../img/productoD.webp";
+
+
+// Datos del slider
+const slidesData = [
+  { id: 1, tipo: "inicio", imagen: logo, slogan: "" },
+  { id: 2, tipo: "producto", imagen: logo2, nombre: "", descripcion: "" },
+  { id: 3, tipo: "producto", imagen: logo3, nombre: "", descripcion: "" },
+];
+
+// Datos para el catalogo de productos
+const productosData = [
+
+  { id: 1, tipo: "producto", imagen: productoA, nombre: "TARUGO PLASTICO NARANJA 3/8",  descripcion:" Accesorios de alta calidad ideales para carpintería y construcción " },
+
+  { id: 2, tipo: "producto", imagen: productoB, nombre: "TORNILLO SPACK 3 X 16 MM (10PCS)", descripcion: " Diseñado para fijaciones seguras en maderas y otros materiales"},
+
+  { id: 3, tipo: "producto", imagen: productoC, nombre: "LLAVE DE GRIFERIA 18 EN 1 MULTIFUNCIONAL", descripcion:"Para tener una mayor eficiencia en baños y cocinas, tubo de abasto y griferia" },
+
+  { id: 4, tipo: "producto", imagen: productoD, nombre: "TUERCA MILIMETRICA M6 (10PCS)", descripcion:"Se utiliza para unir tornillos de la misma medida, es común en carpintería metálica y mobiliario" },
+  
+  // ... más productos
+];
 
 const PaginaInicio = () => {
+
+  // Hooks
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slide = slidesData[currentSlide];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % slidesData.length);
+    }, 6000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="iniciocuerpo">
+    <div className="">
       
-
-      <div className="segundaparte">
-        <div className="tablasranking">
-          <div className="elemento">
-            <Ranking Título={"Cotizaciones más recientes"} />
+        {/* Slider con fondo */}
+        <div className="slider-container">
+          <div
+            className="slider-background"
+            style={{ backgroundImage: `url(${slide.imagen})` }}
+          >
+            <div className="slider-text">
+              {slide.tipo === "inicio" && <h2>{slide.slogan}</h2>}
+              {slide.tipo === "producto" && (
+                <>
+                  <h3>{slide.nombre}</h3>
+                  <p>{slide.descripcion}</p>
+                </>
+              )}
+            </div>
           </div>
 
-          <div className="elemento">
-            <Ranking Título={"Productos más vendidos del 2025-III"} />
+          {/* Puntos de navegación */}
+          <div className="slider-dots">
+            {slidesData.map((_, idx) => (
+              <span
+                key={idx}
+                className={idx === currentSlide ? "active" : ""}
+                onClick={() => setCurrentSlide(idx)}
+              ></span>
+            ))}
           </div>
         </div>
-        <div className="recursos">
-         
+            {/* Catálogo de productos */}
+      <section className="catalogo-productos">
+      {productosData.map((prod) => (
+        <div className="producto-card" key={prod.id}>
+          <img src={prod.imagen} alt={prod.nombre} />
+          <h4>{prod.nombre}</h4>
+          <p>{prod.descripcion}</p>
         </div>
-      </div>
+      ))}
+      </section>
     </div>
   );
 };
