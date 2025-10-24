c71c80b656268d1b9e8b0475b335c5222173aad2
luispal7030 <I202224764@cibertec.edu.pe>
2025-10-24 13:45:31 -0500

    se realiza fusion de archivos en la rama.

--- sce-experiencia/src/assets/components/pages/PaginaInicio.jsx
+++ sce-experiencia/src/assets/components/pages/PaginaInicio.jsx
@@ -1,37 +1,15 @@
 import "../../../css/paginainicio.css";
 import logo from "../../img/torquelogo.jpg";
 import Ranking from "../utils/Ranking.jsx";
-import RecursosVirtuales from "../utils/RecursosVirtuales.jsx";
+import WhatsAppButton from "../WhatsAppButton.jsx";
 
 const PaginaInicio = () => {
   return (
     <div className="iniciocuerpo">
-      <div className="encabezado">
-        <div className="imageninicio">
-          <img className="imagenlogo" src={logo} alt="hola" />
-        </div>
 
-        <div className="reseña">
-          <p>"Empresa dedicada a la venta de Pernería Industrial y </p>
-          <p>elementos de Ferretería. Contamos con más de </p>
-          <p>10 años activos dentro del mercado nacional..."</p>
-        </div>
-      </div>
 
-      <div className="segundaparte">
-        <div className="tablasranking">
-          <div className="elemento">
-            <Ranking Título={"Cotizaciones más recientes"} />
-          </div>
+                <WhatsAppButton />
 
-          <div className="elemento">
-            <Ranking Título={"Productos más vendidos del 2025-III"} />
-          </div>
-        </div>
-        <div className="recursos">
-          <RecursosVirtuales />
-        </div>
-      </div>
     </div>
   );
 };
